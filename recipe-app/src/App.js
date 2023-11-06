1bd4e50d7c1f2ee84d4835089fd8a34513d89b6f
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-11-06 17:17:09 +0000

    initial commit

---
+++ recipe-app/src/App.js
@@ -0,0 +1,12 @@
+import NavBar from "./components/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ recipe-app/src/components/navbar.js
@@ -0,0 +1,11 @@
+
+
+function NavBar() {
+    return (
+        <div className="navbar">
+            <p>Navbar</p>
+        </div>
+    )
+}
+
+export default NavBar;
