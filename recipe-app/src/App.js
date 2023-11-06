937707778df953e142298d592c8a0ce5fbe1f684
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-11-06 17:40:07 +0000

    add navbar and styling

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -3,7 +3,6 @@ import NavBar from "./components/navbar";
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
       <NavBar />
     </div>
   );
--- recipe-app/src/components/navbar.js
+++ recipe-app/src/components/navbar.js
@@ -2,8 +2,13 @@
 
 function NavBar() {
     return (
-        <div className="navbar">
-            <p>Navbar</p>
+        <div className="navbar container">
+            <a href="#!" className="logo"><span>Easy</span>_Recipes</a>
+            <div className="nav-links">
+                <a href="#">Home</a>
+                <a href="#">Recipes</a>
+                <a href="#">Settings</a>
+            </div>
         </div>
     )
 }
