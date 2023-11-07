92af0ca268edd67935314d3d1a23811f7171ed42
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-11-07 20:22:53 +0000

    add sidebar button

--- recipe-app/src/components/navbar.js
+++ recipe-app/src/components/navbar.js
@@ -1,6 +1,7 @@
-
+import { useState } from 'react';
 
 function NavBar() {
+    const [showSideBar, setShowSideBar] = useState(false);
     return (
         <div className="navbar container">
             <a href="#!" className="logo"><span>Easy</span>_Recipes</a>
@@ -8,6 +9,11 @@ function NavBar() {
                 <a href="#">Home</a>
                 <a href="#">Recipes</a>
                 <a href="#">Settings</a>
+            </div>
+            <div onClick={() => setShowSideBar(!showSideBar)} className={showSideBar ? "sidebar-btn active" : "sidebar-btn"}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
             </div>
         </div>
     )
