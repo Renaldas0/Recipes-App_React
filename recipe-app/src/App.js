681e398f84538c3d51177860615ba25654e620f4
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-11-08 19:59:38 +0000

    add hero section and gallery

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,9 +1,13 @@
 import NavBar from "./components/navbar";
+import HeroSection from "./components/hero-section";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="container main">
+        <HeroSection />
+      </div>
     </div>
   );
 }
