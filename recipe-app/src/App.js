36b35748eda6b3a6f448f7bf48649fd7fd6ef336
Renaldas Bendikas <97538312+Renaldas0@users.noreply.github.com>
2023-11-09 11:01:13 +0000

    add improve skills section and styling

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,5 +1,6 @@
 import NavBar from "./components/navbar";
 import HeroSection from "./components/hero-section";
+import ImproveSkills from "./components/improveSkills";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <NavBar />
       <div className="container main">
         <HeroSection />
+        <ImproveSkills />
       </div>
     </div>
   );
