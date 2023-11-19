953233ee1fdc06d4e51ea2c97e3fbfda12cb0660
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-11-19 17:40:10 +0000

    forgot to change a few

--- recipe-app/src/pages/recipes.js
+++ recipe-app/src/pages/recipes.js
@@ -5,42 +5,42 @@ function Recipes() {
     const recipes = [
         {
             title: "Shrimp fried rice",
-            image: "Recipes-App_React/img/food/img1.jpg",
+            image: "./img/food/img1.jpg",
             authorImg: "./img/chefs/chef-morna-tang.jpg",
         },
         {
             title: "Savoury Sweet Pancakes",
-            image: "Recipes-App_React/img/food/img2.jpg",
+            image: "./img/food/img2.jpg",
             authorImg: "./img/chefs/chef-anna-terezevich.jpg",
         },
         {
             title: "Mushroom Noodle Soup",
-            image: "Recipes-App_React/img/food/img3.jpg",
+            image: "./img/food/img3.jpg",
             authorImg: "./img/chefs/chef-morna-tang.jpg",
         },
         {
             title: "Shrimp Noodle Soup",
-            image: "Recipes-App_React/img/food/img4.jpg",
+            image: "./img/food/img4.jpg",
             authorImg: "./img/chefs/chef-morna-tang.jpg",
         },
         {
             title: "Classic Ceasar Salad",
-            image: "Recipes-App_React/img/food/img5.jpg",
+            image: "./img/food/img5.jpg",
             authorImg: "./img/chefs/chef-ann-schvets.jpg",
         },
         {
             title: "Steak",
-            image: "Recipes-App_React/img/food/img6.jpg",
+            image: "./img/food/img6.jpg",
             authorImg: "./img/chefs/chef-vadim-markin.jpg",
         },
         {
             title: "Almost English Breakfast",
-            image: "Recipes-App_React/img/food/img7.jpg",
+            image: "./img/food/img7.jpg",
             authorImg: "./img/chefs/chef-anna-terezevich.jpg",
         },
         {
             title: "Shrimp Spaghetti",
-            image: "Recipes-App_React/img/food/img8.jpg",
+            image: "./img/food/img8.jpg",
             authorImg: "./img/chefs/chef-tirachard-kumtanom.jpg",
         },
     ].sort(() => Math.random() - 0.5);
