386c61cede88a8c0cdd82b47e09605fc434f4c2e
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-11-19 17:30:48 +0000

    attempt different img paths

--- recipe-app/src/components/hero-section.js
+++ recipe-app/src/components/hero-section.js
@@ -2,15 +2,15 @@ import CustomImage from "./customImage";
 
 function HeroSection() {
     const images = [
-        "Recipes-App_React/recipe-app/img/food/img1.jpg",
-        "/Recipes-App_React/recipe-app/img/food/img2.jpg",
-        "./Recipes-App_React/recipe-app/img/food/img3.jpg",
+        "./img/food/img1.jpg",
+        "./img/food/img2.jpg",
+        "./img/food/img3.jpg",
         "/img/food/img4.jpg",
         "./img/food/img5.jpg",
-        "Recipes-App_React/img/food/img6.jpg",
-        "Recipes-App_React/img/food/img7.jpg",
-        "Recipes-App_React/img/food/img8.jpg",
-        "Recipes-App_React/img/food/img9.jpg",
+        "./img/food/img6.jpg",
+        "./img/food/img7.jpg",
+        "./img/food/img8.jpg",
+        "./img/food/img9.jpg",
     ]
     return (
         <div className="section hero">
